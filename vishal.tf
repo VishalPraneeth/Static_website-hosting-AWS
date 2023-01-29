037e03ec746389edf53fa9668dc8a7f60e5ac9ef
Vishal <vishalpraneeth7771@gmail.com>
2023-01-29 19:48:34 +0530

    generated key_pair

--- vishal.tf
+++ vishal.tf
@@ -3,3 +3,30 @@ provider "aws" {
   region  = "ap-south-1"
   profile = "vishal"
 }
+
+
+//Creating Key
+resource "tls_private_key" "tls_key" {
+  algorithm = "RSA"
+}
+
+//Generating Key-Value Pair
+resource "aws_key_pair" "generated_key" {
+  key_name   = "vishal-env-key"
+  public_key = "${tls_private_key.tls_key.public_key_openssh}"
+  
+  depends_on = [
+    tls_private_key.tls_key
+  ]
+}
+
+
+//Saving Private Key PEM File
+resource "local_file" "key-file" {
+  content  = "${tls_private_key.tls_key.private_key_pem}"
+  filename = "vishal-env-key.pem"
+  
+  depends_on = [
+    tls_private_key.tls_key
+  ]
+}
